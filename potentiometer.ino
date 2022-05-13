64d8d6e057382a953e2a350e78be6eb8eeb436da
Remix23 <61829560+Remix23@users.noreply.github.com>
2022-05-13 09:59:22 +0200

    floats -> doubles

--- potentiometer.ino
+++ potentiometer.ino
@@ -18,35 +18,35 @@ const int motor_max = 180;
 
 int height_goal = 250; // in mm
 int tube_height = 500; // in mm 
-float last_err = 0;
-float cum_err = 0;
-float rate_err = 0;
+double last_err = 0;
+double cum_err = 0;
+double rate_err = 0;
 
-int motor_out = 0;
+double motor_out = 0;
 
-float PID_constants [3] = {0, 0, 0}; // respectively for p, i, d
+double PID_constants [3] = {0, 0, 0}; // respectively for p, i, d
 int ball_height;
-int motor_correction;
+double motor_correction;
 unsigned long last_time = 0;
 unsigned long curr_time = 0;
 
-int PID_cycle (int distance, float d_time)
+double PID_cycle (int distance, double d_time)
 {
 
-  float err = height_goal - distance;
+  double err = height_goal - distance;
 
   // PID controls //
 
   // proportional 
-  float p_control = PID_constants[0] * err;
+  double p_control = PID_constants[0] * err;
   // integral 
   cum_err += (err + last_err) * d_time / 2.0f;
-  float i_control = cum_err * PID_constants[1];
+  double i_control = cum_err * PID_constants[1];
   // derrivative 
   rate_err = (err - last_err) / d_time;
-  float d_control = rate_err * PID_constants[2];
+  double d_control = rate_err * PID_constants[2];
 
-  int pid_out = int(p_control + 0 + 0);
+  double pid_out = int(p_control + 0 + 0);
   last_err = err;
   delay(1);
   return pid_out;
@@ -55,11 +55,11 @@ int PID_cycle (int distance, float d_time)
 void updatePIDControls ()
 {
   int p_pot = analogRead(PPotControl);
-  float p_value = float(p_pot) / 1023.0f * 0.01f;
+  double p_value = double(p_pot) / 1023.0f * 0.001f;
   int i_pot = analogRead(IPotControl);
-  float i_value = float(i_pot) / 1023.0f * 0.01f;
+  double i_value = double(i_pot) / 1023.0f * 0.001f;
   int d_pot = analogRead(DPotControl);
-  float d_value = float(d_pot) / 1023.0f * 0.01f;
+  double d_value = double(d_pot) / 1023.0f * 0.001f;
 
   PID_constants[0] = p_value;
   PID_constants[1] = i_value;
@@ -134,7 +134,7 @@ void loop() {
   }
   
   int pot_value = analogRead(heightPotPin);
-  // height_goal = int(float(pot_value) * 500 / 1023);
+  // height_goal = int(double(pot_value) * 500 / 1023);
   
   updatePIDControls();
   printForGraph ();
@@ -145,7 +145,7 @@ void loop() {
 //  Serial.print(" height goal: ");
 //  Serial.println(height_goal);  
 
-  esc.write(motor_out);
+  esc.write((int)motor_out);
   last_time = curr_time;
 
   delay (1);
